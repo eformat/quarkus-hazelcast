0a69f68f1a6a08244e498f1490ae7ec7ad5e5768
Mike Hepburn <eformat@gmail.com>
2019-12-13 14:03:32 +1000

    🐜 refactor 🐜

--- src/main/java/org/acme/APIResource.java
+++ src/main/java/org/acme/APIResource.java
@@ -6,7 +6,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/create")
-public class ExampleResource {
+public class APIResource {
 
     final HazelcastBeanFactory hazelcastBeanFactory = new HazelcastBeanFactory();
 
